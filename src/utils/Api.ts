52f606a07849989a1fc08aa00ee4cccf3217478d
PrettyMuffin <markobrunello2010@gmail.com>
2023-10-01 17:42:36 +0200

    assicurato chiave api con netlify

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,14 +1,15 @@
 import { Review } from "../Models";
 
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const GOOGLE_API_KEY =
+  import.meta.env.VITE_GOOGLE_API_KEY || process.env.GOOGLE_API_KEY;
 
 export async function GetRecensioni() {
   console.log(GOOGLE_API_KEY);
 
   const data = await fetch(
     // heroku app, only in development server, when production, remove
-    // "https://cors-anywhere.herokuapp.com/" +
-    `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJU1RsmiEkeUcR5yG23k0klJI&key=${GOOGLE_API_KEY}&reviews_sort=newest&language=it`,
+    "https://cors-anywhere.herokuapp.com/" +
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJU1RsmiEkeUcR5yG23k0klJI&key=${GOOGLE_API_KEY}&reviews_sort=newest&language=it`,
     {
       mode: "cors",
       headers: {
