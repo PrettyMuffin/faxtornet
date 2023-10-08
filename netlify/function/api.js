0f5f104b941f5f49f693437363a4023769f193ca
PrettyMuffin <markobrunello2010@gmail.com>
2023-10-08 19:06:02 +0200

    removed require(node-fetch) from netlify function

--- netlify/function/api.js
+++ netlify/function/api.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch')
-
 exports.handler = async function(event, context) {
 	const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJU1RsmiEkeUcR5yG23k0klJI&key=${process.env.GOOGLE_API_KEY}&reviews_sort=newest&language=it`;
 
